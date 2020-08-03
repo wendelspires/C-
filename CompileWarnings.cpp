db2be64df8e4d7f0fefac42cc6238cb0c53d30e1
wendelspires <ubuntu@DESKTOP-6JRLH3V.localdomain>
2020-08-03 13:42:48 -0300

    Second Commit

---
+++ CompileWarnings.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(){
+	
+	int favorite_number;
+	
+	favorite_number = 100;
+	
+	std::cout << favorite_number << std::endl;
+	return 0;
+	
+}	
--- First Program.cpp
+++ First Program.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 
 int main()
-{
-		
-		std::cout << "Enter your favorite Number between 1 and 100: ";
-		
+		int favorite_number;
+		std::cout << "Enter your favorite number between 1 and 1000: ";
+		std::cin >> favorite_number;
+		std::cout << "Amazing!!! That's my favorite number too!" << std::endl;
 		return 0;
 }
