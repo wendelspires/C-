9216e1b69c622d412824230b79ceb71948c0f04e
wendelspires <wendelspires@hotmail.com>
2020-08-05 14:57:59 -0300

    Exercise 9 done!

---
+++ Exercise8.cpp
@@ -0,0 +1,17 @@
+//Exercise 8
+//Assignment Operator
+
+#include <iostream>
+
+int main (){
+	
+	num1 = 13;
+	num2 = 0;
+	
+	num1 = 5;
+	
+	num1 = num2;
+	
+	return 0;
+	
+}
---
+++ Exercise9.cpp
@@ -0,0 +1,15 @@
+//Assignment Operator
+//Exercise 9
+
+#include <iostream>
+
+int main (){
+	
+	int number;
+	
+	number = number + 3;
+	number = number + 4;
+	
+	std::cout << "Number: " << number << std::endl;	
+	
+}
