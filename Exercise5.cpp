ce07f109b475fc79c3f06055287f493606696bd4
wendelspires <ubuntu@DESKTOP-6JRLH3V.localdomain>
2020-08-05 10:47:39 -0300

    Exercise5 done!

---
+++ Exercise5.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(){
+	
+	int arr[10] {};
+	arr[0] = 100;
+	arr[9] = 1000;
+	
+	std::cout << "\nThe first element of the array is: " << arr[0] << std::endl;
+	std::cout << "\nThe last element of the array is: " << arr[9] << std::endl;
+}
