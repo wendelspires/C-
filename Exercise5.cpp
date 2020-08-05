c913e1e3b15909e3a0848d28519068c3c8298163
wendelspires <wendelspires@hotmail.com>
2020-08-05 10:51:10 -0300

    Exercise 5

--- Exercise5.cpp
+++ Exercise5.cpp
@@ -4,7 +4,7 @@ int main(){
 	
 	int arr[10] {};
 	arr[0] = 100;
-	arr[9] = 1000;
+	arr[8] = 1000;
 	
 	std::cout << "\nThe first element of the array is: " << arr[0] << std::endl;
 	std::cout << "\nThe last element of the array is: " << arr[9] << std::endl;
