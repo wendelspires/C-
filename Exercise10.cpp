f87d20ef2665bc4edcafc38f4e0acd992bbe3814
wendelspires <wendelspires@hotmail.com>
2020-08-06 15:26:17 -0300

    Exercise 10

---
+++ Exercise10.cpp
@@ -0,0 +1,19 @@
+//Exercise 10
+
+#include <iostream>
+
+int main(){
+	
+	int number {};
+	const int dollar {100};
+	const int quarter {25};
+	const int dime {10};
+	const int nicle {5};
+	const int penny {1};
+	
+	std::cout <<"Enter an amount in cents: " << std::endl;
+	std::cin >> number;
+	
+	
+	
+}
