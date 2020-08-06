2e716ce1eae30608d6df02036f9f772984da778b
wendelspires <wendelspires@hotmail.com>
2020-08-06 15:11:13 -0300

    Logical Operators

--- OperatorPrecedence.cpp
+++ OperatorPrecedence.cpp
@@ -1,5 +1,4 @@
 //Compound assignment Operators
-
 /*
 	a += 1; // a = a + 1;
 	a /= 5; // a = a / 5;
@@ -7,3 +6,19 @@
 
 	cost += items * tax; // cost = cost + (items * tax);
 */
+
+#include <iostream>
+
+int main (){
+	
+	int age {};
+	bool parental_consent {false};
+	bool ssn {false};
+	bool accidents {false};
+
+	if ((age >= 18 || age > 15 && parental_consent) && ssn && !accidents)
+		std::cout << "Yes, you can work! ";
+	
+	return 0;
+
+}
