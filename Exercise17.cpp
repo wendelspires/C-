2f62a2af242d9791a08a1e23b20a755ff4570b83
wendelspires <wendelspires@hotmail.com>
2020-08-11 11:04:42 -0300

    Exercise Loops

--- Exercise17.cpp
+++ Exercise17.cpp
@@ -2,11 +2,13 @@
 
 #include <iostream>
 #include <vector>
+#include <iomanip>
 
 int main (){
 	
 	char selection {};
-	int number {};
+	std::vector <int> number {};
+	int num_to_add {};
 	
 	do {
 		std::cout << "\n----------------------" << std::endl;
@@ -19,27 +21,74 @@ int main (){
 		std::cout << "\nEnter in your selection: ";
 		std::cin >> selection;
 		
-		if (selection == 'P' || selection == 'p')
-			if (number != 0)
-				std::cout << "[ " << number << " ] " << std::endl;
-			else
-				std::cout << "The list is empty" << std::endl;
 		
+		
+		if (selection == 'P' || selection == 'p'){
+			if (number.size() == 0)
+				std::cout << "[] The list is empty" << std::endl;
+			else{
+				std::cout << "[";
+				for(auto num:number)
+					std::cout <<" " << num << " ";
+				std::cout << "]";
+			}
+		}
+
 		else if (selection == 'A' || selection == 'a'){
+			int num_to_add {};
 			std::cout << "Add a number: ";
-			std::cin >> number;
+			std::cin >> num_to_add ;
+			number.push_back(num_to_add);
 		}
 		
 		else if (selection == 'M' || selection == 'm'){
-			std::cout << "The mean of the numbers: " << std::endl;
+			
+			if (number.size() == 0)
+				std::cout << "\nUnable to calculate the mean, no data" << std::endl;
+			
+			else{
+			double average {};
+			double total {};
+			
+			for (auto num:number)
+				total += num;
+			
+			if (number.size() != 0)
+				average = total/number.size();
+				
+				std::cout << std::fixed << std::setprecision(1);
+				std::cout << "Average of the numbers is: " << average << std::endl;
+			}	
 		}
 		
+		//Smalles Number
 		else if (selection == 'S' || selection == 's'){
 			std::cout << "Smallest number: " << std::endl;
+			
+			if (number.size() == 0)
+				std::cout << "\nUnable do determine the smallest number, list is empty" << std::endl;
+			
+			else{
+				int smallest = number.at(0);
+				for (auto num:number)
+					if (num < smallest)
+					smallest = num;	
+				std::cout << "The smallest number is: " << smallest << std::endl;
+				}
 		}
 		
 		else if (selection == 'L' || selection == 'l'){
-			std::cout << "Largest number: " << std::endl;
+			
+			if (number.size() == 0)
+				std::cout << "\nUnable do determine the smallest number, list is empty" << std::endl;
+			
+			else{
+				int largest = number.at(0);
+				for (auto num:number)
+					if (num > largest)
+					largest = num;	
+				std::cout << "The Largest number is: " << largest << std::endl;
+				}
 		}
 		
 		else if (selection == 'Q' || selection == 'q'){
